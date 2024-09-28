8bba61d14b04df73ec5608282b2d431c4cc80fba
anujkv <anujkrvaish@gmail.com>
2024-09-28 10:16:53 +0530

    Registration UI done

--- app/src/main/java/com/net/todoapplication/view/auth/RegistrationScreen.kt
+++ app/src/main/java/com/net/todoapplication/view/auth/RegistrationScreen.kt
@@ -1,7 +1,151 @@
 package com.net.todoapplication.view.auth
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.dewan.todoapp.util.Validator
+import com.net.todoapplication.R
+import com.net.todoapplication.utils.ClassName
 
 @Composable
-fun RegistrationScreen(navController: NavController){}
+fun RegistrationScreen(navController: NavController) {
+    var emailOrPhone by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var confirmPassword by remember { mutableStateOf("") }
+    var isLoading by remember { mutableStateOf(false) }
+
+    var isEmailValid by remember { mutableStateOf(true) }
+    var isPasswordValid by remember { mutableStateOf(true) }
+    var isConfirmPasswordValid by remember { mutableStateOf(true) }
+    val context = LocalContext.current
+
+    Box(
+        Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "LogoImage",
+                Modifier.size(80.dp)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedTextField(
+                value = emailOrPhone,
+                onValueChange = {
+                    emailOrPhone = it
+                    isEmailValid = Validator.validateEmail(it)
+                },
+                label = { Text(text = "Email or Phone") },
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                isError = !isEmailValid
+
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedTextField(
+                value = password,
+                onValueChange = {
+                    password = it
+                    isPasswordValid = Validator.validatePassword(it)
+                },
+                label =  { Text(text = "Password") },
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                isError = !isPasswordValid
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedTextField(
+                value = confirmPassword,
+                onValueChange = {
+                    confirmPassword = it
+                    isConfirmPasswordValid = it == password},
+                label = {Text(text = "Confirm Password")},
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                isError = !isConfirmPasswordValid)
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(onClick = {
+                if (isEmailValid && isPasswordValid && isConfirmPasswordValid) {
+                    isLoading = true
+
+
+                }
+            },
+                modifier = Modifier.padding(start = 20.dp, end = 20.dp),
+                enabled = isEmailValid && isPasswordValid && isConfirmPasswordValid
+            ){
+                Text(text = "Register")
+            }
+
+            Spacer(modifier = Modifier.height(25.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+
+                Text(text = "Already Account? ")
+
+                Text(
+                    text = "Login",
+                    color = colorResource(id = R.color.blue),
+                    textDecoration = TextDecoration.Underline,
+                    modifier = Modifier.clickable {
+                        navController.navigate(ClassName.LOGIN_SCREEN)
+                    })
+            }
+        }
+    }
+
+}
+
+@Preview
+@Composable
+fun RegistrationScreenPreview() {
+    val navController = rememberNavController()
+    RegistrationScreen(navController)
+}
